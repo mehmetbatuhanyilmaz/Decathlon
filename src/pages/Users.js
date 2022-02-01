503826915640ca5b925bd67ca118e47b7b0cb23a
Mehmet Batuhan Yılmaz <84319499+mehmetbatuhanyilmaz@users.noreply.github.com>
2022-02-01 17:17:06 +0300

    Some changes added

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -2,16 +2,19 @@ import React from 'react';
 import '../App';
 import '../App.css';
 import image from "../images/background2.gif";
+import Cards from "../components/Cards/index";
+
+
+
+
+
 export default function Users() {
   return (
     <>
      
     <div className='users' style={{ backgroundImage: `url(${image})` }}>
-
-
-
-
-    <br/><br/><br/><br/><br/><br/><br/>
+    
+     <br/><br/><br/><br/><br/><br/>
     </div>
      
 
@@ -19,3 +22,7 @@ export default function Users() {
     </>
   );
 }
+
+
+
+
